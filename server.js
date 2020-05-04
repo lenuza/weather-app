1bd3e2ccbbbbf587dc0ee74d6b6882b7fd609dcd
Elena Nedkova <lenuza@gmail.com>
2020-05-04 18:37:02 +0300

    added main app folder and files and pointed express to it

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+/* Initializing the main project folder */
+app.use(express.static('weather-app'));
 
-
+// API endpoint
 projectData = {};
+
+// set port
+
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`Hello ${port}`)
+}
+
 
 
 
