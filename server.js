58dbbe75cb19813bb6e4cc0062c8cb198e3700d1
Elena Nedkova <lenuza@gmail.com>
2020-05-06 15:25:38 +0300

    set port const

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ app.use(express.static('app'));
 const projectData = {};
 
 // set port
-const port = 8000;
+const port = process.env.port || 8000;
 const server = app.listen(port, () => {
     console.log(`Hello, listening on port ${port}`);
 });
 
 const apiKey = 'bbcbc22c4ee1c4bd5eecd122afbb2825';
+
+// app.get('/all', function (req, res) {
+//     res.send(projectData);
+//     // console.log('hello world')
+// })
 
 app.get('/', function (req, res) { res.send('welcome!'); });
 
